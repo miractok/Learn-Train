12b00419556178f4a0f3007dc80112d8d59f3c20
Unknown <francoisphilippe2004@gmail.com>
2019-03-28 10:51:08 +0100

    JavaScript début
    JavaScript début

---
+++ java.js
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+
+<head>
+     <title>Premier script en JavaScript</title>
+     <meta http-equiv="Content-Type" content="text/html"/>
+     <meta http-equiv="Content-Script-Type" content="text/javascript"/>
+</head>
+
+<body>
+     <script type='text/JavaScript'>
+	 //<![CDATA[
+	        
+    //]]>
+    </script>
+</body>
+
+</html>
